57b4683f782900aab60705f502205f3d5d09133f
shachar <shachco@post.bgu.ac.il>
2025-01-17 19:51:54 +0200

    provengo continue

--- Provengo/helloprovengo/spec/js/behavior.js
+++ Provengo/helloprovengo/spec/js/behavior.js
@@ -1,18 +1,25 @@
 /* @provengo summon selenium */
 
-// Scenario: User buys an item from the store
-bthread('User buys an item from the store', function () {
+bthread('', function () {
   let userSession = new SeleniumSession('user_session');
+  let adminSession = new SeleniumSession('admin_session');
 
-  // User logs in
   userSession.start(URL)
+  adminSession.start(ADMIN_URL);
+
   goToLogin(userSession)
-  login(userSession, userDetails.user.email, userDetails.user.password)
+  userLogin(userSession, userDetails.user.email, userDetails.user.password)
   addItemToCart(userSession)
   proceedToCheckout(userSession)
-  // personalInformation(userSession, userDetails.user.firstName, userDetails.user.lastName, userDetails.user.email, userDetails.user.password, userDetails.user.birthdate)
   addresses(userSession, userDetails.user.address, userDetails.user.city, userDetails.user.state, userDetails.user.zipCode, userDetails.user.country)
   shippingMethod(userSession)
   payment(userSession)
   orderConfirmation(userSession)
+
+  addItemToCart(userSession)
+
+  adminLogin(adminSession, userDetails.admin.email, userDetails.admin.password)
+  deleteItem(adminSession)
+
+  tryToBuyProduct(userSession) 
 });
