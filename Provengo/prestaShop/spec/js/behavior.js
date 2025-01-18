2edc8dd7d59843e88c46dcb13cf48e22eeccda6b
shachar <shachco@post.bgu.ac.il>
2025-01-18 23:28:31 +0200

    maximize

--- Provengo/prestaShop/spec/js/behavior.js
+++ Provengo/prestaShop/spec/js/behavior.js
@@ -1,14 +1,26 @@
 /* @provengo summon selenium */
 
-bthread('user buys product', function () {
-  let s = new SeleniumSession('user_session');
-  s.start(URL)
-  buyProduct(s)
+let userSession = new SeleniumSession('user_session');
+let adminSession = new SeleniumSession('admin_session');
+
+bthread('buy product', function () {
+  userSession.start(URL)
+  userLogin(userSession)
+  addProductToCart(userSession)
+  proceedToCheckout(userSession)
+  checkout(userSession)
 });
 
-bthread('admin deletes product', function () {
-  let s = new SeleniumSession('admin_session');
-  s.start(ADMIN_URL);
-  deleteProduct(s)
+bthread('delete product', function () {
+  adminSession.start(ADMIN_URL);
+  adminLogin(adminSession)
+  searchProduct(adminSession)
+  deleteProduct(adminSession)
 });
 
+bthread('correction', function () {
+  sync({
+    WaitFor: Event('End(checkout)', {EndEvent: true, session: userSession}),
+    block: Event('Start(deleteProduct)', {StartEvent: true, session: adminSession})
+  })
+})
